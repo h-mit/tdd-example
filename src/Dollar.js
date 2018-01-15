d1f238ca54f28e52a851e313ae6a1129e4a7cbf5
Hiroshi MITANI <hmitani0429@gmail.com>
2018-01-16 00:03:13 +0900

    コードにesdocコメントを追加

--- src/Dollar.js
+++ src/Dollar.js
@@ -1,8 +1,20 @@
+/**
+ * ドルを扱うクラス
+ */
 class Dollar {
+  /**
+   * ドルを指定して初期化する
+   * @param {Number} amount ドル
+   */
   constructor (amount) {
     this.amount = amount
   }
   
+  /**
+   * ドルの乗算を行うメソッド
+   * @param {Number} multiplier 乗算する数値
+   * @returns {Dollar} 新しい `Dollar` インスタンスを返す
+   */
   times (multiplier) {
     return new Dollar(this.amount * multiplier)
   }
--- test/specs/Doller.test.js
+++ test/specs/Doller.test.js
@@ -1,10 +1,18 @@
 import Dollar from '@/Dollar'
 
-test('ドルの乗算', () => {
-  const five = new Dollar(5)
-  let product
-  product = five.times(2)
-  expect(product.amount).toBe(10)
-  product = five.times(3)
-  expect(product.amount).toBe(15)
+/** @test {Dollar} */
+describe('Dollar', () => {
+  /** @test {Dollar#times} */
+  test('5ドルに2を掛けると10ドルのインスタンスを返す', () => {
+    const five = new Dollar(5)
+    let product = five.times(2)
+    expect(product.amount).toBe(10)
+  })
+  
+  /** @test {Dollar#times} */
+  test('乗算後に元のインスタンスの値に変更がない', () => {
+    const five = new Dollar(5)
+    five.times(2)
+    expect(five.amount).toBe(5)
+  })
 })
