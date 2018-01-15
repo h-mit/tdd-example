dd1fdd1d5a61859f571bab807e7d8a42a5f3853b
Hiroshi MITANI <hmitani0429@gmail.com>
2018-01-16 00:49:16 +0900

    equalsメソッドを追加

--- src/Dollar.js
+++ src/Dollar.js
@@ -4,7 +4,7 @@
 class Dollar {
   /**
    * ドルを指定して初期化する
-   * @param {Number} amount ドル
+   * @param {number} amount ドル
    */
   constructor (amount) {
     this.amount = amount
@@ -12,11 +12,20 @@ class Dollar {
   
   /**
    * ドルの乗算を行うメソッド
-   * @param {Number} multiplier 乗算する数値
+   * @param {number} multiplier 乗算する数値
    * @returns {Dollar} 新しい `Dollar` インスタンスを返す
    */
   times (multiplier) {
     return new Dollar(this.amount * multiplier)
+  }
+
+  /**
+   * 等価比較を行うメソッド
+   * @param {Dollar} object 比較対象の `Dollar` インスタンス
+   * @returns {boolean} 等価であれば `true` を返す
+   */
+  equals (object) {
+    return this.amount === object.amount
   }
 }
 
--- test/specs/Doller.test.js
+++ test/specs/Doller.test.js
@@ -2,17 +2,24 @@ import Dollar from '@/Dollar'
 
 /** @test {Dollar} */
 describe('Dollar', () => {
+  const five = new Dollar(5)
+
   /** @test {Dollar#times} */
-  test('5ドルに2を掛けると10ドルのインスタンスを返す', () => {
-    const five = new Dollar(5)
-    let product = five.times(2)
+  test('times()', () => {
+    let product
+    // 5ドルに2を掛けると10ドルのインスタンスを返す
+    product = five.times(2)
     expect(product.amount).toBe(10)
+    // 5ドルに3を掛けると15ドルのインスタンスを返す
+    product = five.times(3)
+    expect(product.amount).toBe(15)
   })
   
-  /** @test {Dollar#times} */
-  test('乗算後に元のインスタンスの値に変更がない', () => {
-    const five = new Dollar(5)
-    five.times(2)
-    expect(five.amount).toBe(5)
+  /** @test {Dollar#equals} */
+  test('equals()', () => {
+    // 5ドルと5ドルの等価比較でtrueを返す
+    expect(five.equals(new Dollar(5))).toBeTruthy()
+    // 5ドルと6ドルの等価比較でfalseを返す
+    expect(five.equals(new Dollar(6))).toBeFalsy()
   })
 })
