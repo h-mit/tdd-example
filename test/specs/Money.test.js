ce1abf20c7010659e28e7c991b3322cd2fbbb4ff
Hiroshi MITANI <hmitani0429@gmail.com>
2018-01-18 01:23:25 +0900

    冗長なテストを削除

--- test/specs/Money.test.js
+++ test/specs/Money.test.js
@@ -6,16 +6,12 @@ describe('Money', () => {
   test('times()', () => {
     expect(Money.dollar(5).times(2).equals(Money.dollar(10))).toBeTruthy()
     expect(Money.dollar(5).times(3).equals(Money.dollar(15))).toBeTruthy()
-    expect(Money.franc(5).times(2).equals(Money.franc(10))).toBeTruthy()
-    expect(Money.franc(5).times(3).equals(Money.franc(15))).toBeTruthy()
   })
 
   /** @test {Money#equals} */
   test('equals()', () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy()
     expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy()
-    expect(Money.franc(5).equals(Money.franc(5))).toBeTruthy()
-    expect(Money.franc(5).equals(Money.franc(6))).toBeFalsy()
     expect(Money.dollar(5).equals(Money.franc(5))).toBeFalsy()
   })
 
